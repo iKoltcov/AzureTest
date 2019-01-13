19bd2a4ca3caee4e302b0f5444363bb2b0c128ab
Ivan Koltcov <ikoltcov@yandex.ru>
2019-01-13 11:47:24 +0300

    master // get sha256 of invocationId

--- AzureTest/Functions/HttpTriggers/Function.cs
+++ AzureTest/Functions/HttpTriggers/Function.cs
@@ -7,6 +7,8 @@ using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 using AzureTest.Functions.Models;
+using System.Security.Cryptography;
+using System.Text;
 
 namespace AzureTest
 {
@@ -20,18 +22,25 @@ namespace AzureTest
         {
             try
             {
-                string requestBody = new StreamReader(request.Body).ReadToEnd();
-                FunctionModel httpTriggerModel = JsonConvert.DeserializeObject<FunctionModel>(requestBody);
-
-                return string.IsNullOrWhiteSpace(httpTriggerModel.Name)
-                    ? new BadRequestObjectResult("Please pass a 'Name' in the request body")
-                    : (ActionResult)new OkObjectResult($"Hello, {httpTriggerModel.Name}");
+                using (var sha256 = SHA256.Create())
+                {
+                    var hash = sha256.ComputeHash(executionContext.InvocationId.ToByteArray());
+                    return new OkObjectResult(ToHex(hash));
+                }
             }
             catch(Exception exception)
             {
                 log?.LogError("[{0}:{1}] {2}", executionContext.InvocationId, executionContext.FunctionName, exception.Message);
-                return new OkObjectResult(exception.Message);
+                return new BadRequestObjectResult(exception.Message);
             }
+        }
+
+        private static string ToHex(byte[] bytes, bool upperCase = false)
+        {
+            StringBuilder result = new StringBuilder();
+            for (int i = 0; i < bytes.Length; i++)
+                result.Append(bytes[i].ToString(upperCase ? "X2" : "x2"));
+            return result.ToString();
         }
     }
 }
